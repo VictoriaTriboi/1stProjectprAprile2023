c9d1ed740146240894a36d11a0f407a41ed0fd11
VictoriaTriboi <victoriatriboi16@gmail.com>
2023-10-08 23:31:20 -0500

    committed

---
+++ src/gitHub/gitTest.java
@@ -0,0 +1,12 @@
+package gitHub;
+
+public class gitTest {
+
+	public static void main (String[] args) {
+		printMe();
+	}
+	
+	public static void  printMe () {
+		System.out.println("git test");
+	}
+}
